6e1af46bbc99e9da89c1a76d7ed29c006d0b08e4
Ari-Ario <xusrew.z@gmail.com>
2023-12-21 17:12:19 +0100

    the first neutral face finished

---
+++ script.js
@@ -0,0 +1,7 @@
+
+function toHappy(){
+    const sectionCurrent = document.getElementById("neutral");
+    sectionCurrent.style.display= "none"
+    const sectionHappy = document.getElementById("smile");
+    sectionHappy.style.display= "block"
+}
